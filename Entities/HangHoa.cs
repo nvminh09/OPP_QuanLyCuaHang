6466ea3e55765309df267c5be0daf5ac881f43fc
nvminh09 <nvminh09@gmail.com>
2023-05-06 11:01:16 +0700

    first commit

---
+++ Entities/HangHoa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Entities
+{
+    public class HangHoa
+    {
+        public string MaHang { get; set; }
+        public string TenHang { get; set; }
+        public string LoaiHang { get; set; }
+        public int HanDung { get; set; }
+        public string CongTy { get; set; }
+        public int NamSanXuat { get; set; }
+        public int GiaBan { get; set; }
+        public HangHoa()
+        {
+        }
+        public HangHoa(string ma, string ten, string loai,
+                        int han, string cty, int nam, int gia)
+        {
+            if (string.IsNullOrWhiteSpace(ma) || string.IsNullOrWhiteSpace(ten) ||
+                string.IsNullOrWhiteSpace(loai) || string.IsNullOrWhiteSpace(cty) ||
+                han <= 0 || nam < 0 || gia < 0)
+            {
+                throw new Exception("Hang hoa da nhap khong hop le.");
+            }
+            MaHang = ma;
+            TenHang = ten;
+            LoaiHang = loai;
+            HanDung = han;
+            CongTy = cty;
+            NamSanXuat = nam;
+            GiaBan = gia;
+        }
+    }
+}
---
+++ OOP_QuanLyCuaHang/Pages/MH_DanhSachHangHoa.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOP_QuanLyCuaHang.Pages
+{
+    public class MH_DanhSachHangHoaModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
